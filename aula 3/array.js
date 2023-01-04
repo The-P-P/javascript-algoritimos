38ff358d579b61b27dbb7c3d1afeb7c468da5f88
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2023-01-04 19:54:47 -0300

    commit ...

---
+++ aula 3/array.js
@@ -0,0 +1,49 @@
+const listaLivros = [
+    {
+        nome:"PHP",
+        preco: 56
+    },
+    {
+        nome: "JavaScript",
+        preco: 27
+    },
+    {
+        nome: "Java",
+        preco: 89
+    },
+    {
+        nome: "Go",
+        preco: 47
+    },
+    {
+        nome: "Elixir",
+        preco: 46
+    },
+
+    {
+        nome: "Python",
+        preco: 30
+    },
+    {
+        nome:"Rust",
+        preco: 22
+    },
+    {
+        nome: "Ruby",
+        preco: 26
+    },
+    {
+        nome: "C#",
+        preco: 37
+    },
+    {
+        nome: "C++",
+        preco: 38
+    },
+    {
+        nome: "Scala",
+        preco: 42
+    }
+]
+
+module.exports = {listaLivros}
